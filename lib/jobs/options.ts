4383f996fc9dc000a1f12f0b115c677fc9ac8077
Mike Graham <mikeg1219@yahoo.com>
2026-03-29 11:51:25 -0400

    fix line types

--- lib/jobs/options.ts
+++ lib/jobs/options.ts
@@ -1,17 +1,16 @@
-
 export const LINE_TYPES = [
-"General Boarding",
-"Check-In",
-"Bag Drop",
-"TSA PreCheck",
+  "General Boarding",
+  "Check-In",
+  "Bag Drop",
+  "TSA PreCheck",
 ] as const;
 export type LineType = (typeof LINE_TYPES)[number];
 export const ESTIMATED_WAIT_OPTIONS = [
-"15 min",
-"30 min",
-"45 min",
-"1 hour",
-"1.5 hours",
-"2+ hours",
+  "15 min",
+  "30 min",
+  "45 min",
+  "1 hour",
+  "1.5 hours",
+  "2+ hours",
 ] as const;
 export type EstimatedWait = (typeof ESTIMATED_WAIT_OPTIONS)[number];
